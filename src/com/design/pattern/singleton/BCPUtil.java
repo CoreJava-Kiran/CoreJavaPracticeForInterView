812f8177ced8291dac09b247bf7bc9d50fe2bf6e
Kiran <kiranreddyamk@gmail.com>
2017-09-17 15:20:25 -0400

    Fix access moidfiers

--- src/com/design/pattern/singleton/BCPUtil.java
+++ src/com/design/pattern/singleton/BCPUtil.java
@@ -2,9 +2,10 @@ package com.design.pattern.singleton;
 
 public class BCPUtil {
 	
+	//This is thread safe as we are creating at the class-load time
 	private static BCPUtil instance = new BCPUtil();
 	
-	private BCPUtil(){
+	protected BCPUtil(){
 		
 	}
 	
